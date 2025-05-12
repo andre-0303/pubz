dd2b688864876d3f76f0d9f7d9bfe7d13326a807
andre-0303 <bandeiradev.ti@gmail.com>
2025-05-12 10:53:40 -0300

    :sparkles: Adiciona Especialidades

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/Banner";
+import Especialidades from "@/components/Especialidades";
 
 export default function Home() {
   return (
     <>
     <Banner/>
+    <Especialidades/>
     </>
   );
 }
---
+++ src/components/Especialidades.tsx
@@ -0,0 +1,10 @@
+export default function Especialidades() {
+    return (
+        <>
+            <div className="px-8 py-4 h-[600px] flex justify-center bg-[#f5f5dc]">
+                <h1 className="text-4xl font-serif font-semibold mt-4 uppercase">Nossas <span className="text-red-500">especialidades</span></h1>
+
+            </div>
+        </>
+    )
+}
